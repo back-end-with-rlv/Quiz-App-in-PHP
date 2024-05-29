c79af82cd55922711c2e30d24d341e0637b47f31
CodeWithrlv <129599537+back-end-with-rlv@users.noreply.github.com>
2024-05-29 09:04:24 +0530

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,12 @@
+<footer class="container-fluid text-center">
+  <p>© 2024 My Website. All rights reserved.</p>
+  <ul class="list-inline">
+    <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+    <li class="list-inline-item"><a href="#">Terms of Use</a></li>
+    <li class="list-inline-item"><a href="#">Contact Us</a></li>
+  </ul>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
